5ca43affcf95117b4c8885a35a31e938c7b9f309
Joseph Cittadino <jcittadino@gmail.com>
2020-09-25 08:59:27 -0400

    added basic arithemtic with user input

--- python-calculator.py
+++ python-calculator.py
@@ -1,5 +1,14 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter a number: ")
-result = float(num1) + float(num2)
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
 
-print(result)
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
